dab770d2198a8da1e7288fa8aada817da42d1925
Fernando <fadagoberto@gmail.com>
2018-10-25 15:24:05 -0300

    Login Page

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,22 @@
+﻿namespace Lands.Infrastructure
+{
+    using ViewModels;
+    
+    public class InstanceLocator
+    {
+
+        #region Property
+        public MainViewModel Main{ get; set; }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+
+
+
+    }
+}
